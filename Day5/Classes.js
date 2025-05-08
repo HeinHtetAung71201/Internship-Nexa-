fa93a76358f60a1eb17649d89bbc30d356d80946
HeinHtetAung71201 <htethein609@gmail.com>
2025-05-08 17:33:09 +0630

    day5Classes

---
+++ Day5/Classes.js
@@ -0,0 +1,66 @@
+// BluePrints for creating objects
+//Basic Syntax
+class Person{
+    //constructor for initialization used as arguments in function
+    constructor(name,age){
+        this.namae=name;
+        this.nen=age;
+    }
+    //Greet Method
+    greet(){
+        console.log(`Mingalar Par ${this.namae}`);
+    }
+}
+
+const user1= new Person("Heintet",23);
+// console.log(user1);
+// console.log(user1.namae);
+// console.log(user1.greet());
+// console.log(typeof(user1));
+
+//Class with getter and setter
+class student{
+    constructor(name,grade) {
+        this.name=name;
+        this._grade=grade;//_grade for private varible
+    }
+    //getter
+    get grade(){
+        return this._grade;
+    }
+    //setter
+    set grade(value){
+        if(value<0 || value >100){
+            console.error("Grade must be between 0 and 100.");
+        }
+        else{
+            this._grade = value;
+        }
+    }
+    
+}
+//Building object using student class
+    const student1=new student("KoHein",40);
+    // console.log(student1.grade);
+    // student1.grade(30);
+    student1.grade=30;
+    // student1.grade=120;
+    // console.log(student1.grade);
+
+//StaticMethods
+//Belongs to the class itself, not to the instance of class
+class MathHelper {
+    static square(number) {
+      return number * number;
+    }
+  
+    static cube(number) {
+      return number * number * number;
+    }
+  }
+//   does not need to make instance for class
+//   console.log(MathHelper.square(5)); // Output: 25
+//   console.log(MathHelper.cube(3));   // Output: 27
+  
+    
+
--- Day5/JSON.js
+++ Day5/JSON.js
@@ -22,17 +22,51 @@ const jString= JSON.stringify(obj,null,2);
 // console.log(typeof(jString));
 
 //json with fetching API
-async function jewl(){
-        try{
-            const res= await fetch('https://fakestoreapi.com/products/category/jewelery');
-            const data= await(res.json());
-            console.log(data);
-            data.map(cate=>{
-                console.log(cate.title);
-            })
-        }
-        catch(error){
-            console.log("Error: ",error);
-        }
+// async function jewl(){
+//         try{
+//             const res= await fetch('https://fakestoreapi.com/products/category/jewelery');
+//             const data= await(res.json());
+//             console.log(data);
+//             data.map(cate=>{
+//                 console.log(cate.title);
+//             })
+//         }
+//         catch(error){
+//             console.log("Error: ",error);
+//         }
+//     }
+//     jewl();
+
+const users = [
+    { "name": "Alice", "age": 25 },
+    { "name": "Bob", "age": 20 },
+    { "name": "Charlie", "age": 30 }
+  ];
+  
+  const youngUsers = users.filter(user => user.age < 30);
+//   console.log(youngUsers);
+  
+const prof = { "name": "Alice", "age": 25 };
+prof.age = 26;
+// console.log(typeof(prof));
+// console.log(JSON.stringify(prof));
+// console.log(typeof(prof));
+
+//Nested JSON
+const usersData = {
+    "name": "Alice",
+    "address": {
+      "city": "New York",
+      "zip": "10001"
     }
-    jewl();
+  };
+  
+  // Accessing nested values
+  console.log(usersData.address.city); // Output: "New York"
+  
+  // Modifying nested values
+  usersData.address.city = "Los Angeles";
+  console.log(JSON.stringify(usersData));
+  
+
+// Output: {"name":"Alice","age":26}
